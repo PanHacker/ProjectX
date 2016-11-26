99dbdd4ee9b4e1ddccc957e1d82dd13adcbc2369
zekori96 <tobiasz.rumian@hotmail.com>
2016-11-26 16:02:40 +0100

    Cleaning time

--- src/MainGui.java
+++ src/MainGui.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,7 +9,7 @@ import java.util.Date;
 /**
  * Created by zekori on 25.11.16.
  */
-public class MainGui extends JFrame{
+public class MainGui extends JFrame implements KeyListener{
 
     private JPanel mainPanel;
     private JButton rozpocznijButton;
@@ -19,29 +21,30 @@ public class MainGui extends JFrame{
     private JLabel czasOdRozpoczecia;
     private JTextField a47SygnałyWytwarzaneWTextField;
     private JPanel questionPanel;
-    private Tabelka tabelka;
 
-    public MainGui(){
+    public MainGui() {
 
         setContentPane(mainPanel);
-        pack();
+        addKeyListener(this);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         questionPanel.setVisible(false);
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
-        tabelka = new Tabelka();
+        this.setLocation(Settings.SCREEN_DIMENSION.width / 2 - this.getSize().width / 2, Settings.SCREEN_DIMENSION.height / 2 - this.getSize().height / 2);
         ResourceManager resourceManager = new ResourceManager(this);
         resourceManager.start();
         zatwierdźButton.setEnabled(false);
+        pack();
         this.setVisible(true);
 
         rozpocznijButton.addActionListener(actionEvent -> {
             questionPanel.setVisible(true);
-            Timekeeper.createClockThread(this,1);
+            Timekeeper.createClockThread(this, 1);
             rozpocznijButton.setVisible(false);
             wynikiButton.setVisible(false);
         });
-        wynikiButton.addActionListener(actionEvent -> tabelka.setVisible(true));
+        wynikiButton.addActionListener(actionEvent -> {
+            Score score = new Score(getThis());
+            score.setVisible(true);
+        });
         zatwierdźButton.addActionListener(actionEvent -> {
             TakJakPanJezusPowiedzial tak = new TakJakPanJezusPowiedzial();
             tak.start();
@@ -49,40 +52,72 @@ public class MainGui extends JFrame{
         });
         radio3.addActionListener(actionEvent -> {
 
-            if(radio3.isSelected()){
+            if (radio3.isSelected()) {
                 radio3.setSelected(true);
                 radio2.setSelected(false);
                 radio1.setSelected(false);
             }
         });
         radio2.addActionListener(actionEvent -> {
-            if(radio2.isSelected()){
+            if (radio2.isSelected()) {
                 radio2.setSelected(true);
                 radio1.setSelected(false);
                 radio3.setSelected(false);
             }
         });
         radio1.addActionListener(actionEvent -> {
-            if(radio1.isSelected()){
+            if (radio1.isSelected()) {
                 radio1.setSelected(true);
                 radio2.setSelected(false);
                 radio3.setSelected(false);
             }
         });
     }
-    public void setCzasOdRozpoczecia(long time){
+
+    public void setCzasOdRozpoczecia(long time) {
         Date date = new Date(time);
         DateFormat formatter = new SimpleDateFormat("mm:ss");
-        czasOdRozpoczecia.setText("Czas zadania:"+formatter.format(date));
+        czasOdRozpoczecia.setText("Czas zadania:" + formatter.format(date));
     }
-    private MainGui getThis(){
+
+    private MainGui getThis() {
         return this;
     }
-    public void setZatwierdźButtonEnabled(boolean b){
+
+    public void setZatwierdźButtonEnabled(boolean b) {
         zatwierdźButton.setEnabled(b);
     }
 
     public static void main(String[] args) {
         new MainGui();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //if (e.isShiftDown() && e.isControlDown() && e.isAltDown() && e.getKeyCode() == KeyEvent.VK_UP) System.err.println("działam");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.err.println("działam");
+        //TODO: Naprawić
+    }
+}
+class Score extends JDialog {
+    Score(JFrame owner){
+        super(owner, "Wyniki", true);
+        setLocationRelativeTo(null);
+        this.setLocation(Settings.SCREEN_DIMENSION.width/2-this.getSize().width/2-453, Settings.SCREEN_DIMENSION.height/2-this.getSize().height/2);
+        JLabel label = new JLabel("Nie posiadasz jeszcze zapisanych wyników.");
+        label.setFont(new Font("Serif", Font.PLAIN, 48));
+        add(label,BorderLayout.CENTER);
+        JButton ok = new JButton("ok");
+        ok.addActionListener(e -> setVisible(false));
+        add(ok, BorderLayout.SOUTH);
+        setSize(906, 200);
     }
 }
--- src/Settings.java
+++ src/Settings.java
@@ -7,5 +7,5 @@ public class Settings {
     public static final Dimension SIZE_OF_TROLL_WINDOW = new Dimension(400,400);
     public static final int DELTA_TIME_OF_WINDOW_MOVE = 100;
     public static final Point START_MOVE_OF_WINDOW = new Point(100,100);
-
+    public static final Dimension SCREEN_DIMENSION = Toolkit.getDefaultToolkit().getScreenSize();
 }
